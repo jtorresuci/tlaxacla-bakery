92f92b36e5e4b3636a78b1b664fc5642ec2b5666
Jose Torres <jtorresocc@gmail.com>
2023-02-22 22:15:40 -0800

    Updated Site.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,23 @@ const googleAnalytics = process.env.REACT_APP_GOOGLE_ANALYTICS_ID;
 function App() {
   return (
     <div className="container shadow-lg p-3 mt-5 mb-5 ml-1 mr-1 bg-light rounded">
-    <GoogleAnalytics trackingId={googleAnalytics} />
+      <GoogleAnalytics trackingId={googleAnalytics} />
       {/* <Header logo="concha.png" companyName="Tlaxcala Bakery" /> */}
       <Hero />
       <Product
         title="Freshly Made"
-        description="At Tlaxcala Bakery, all of our breads and cakes are freshly made daily for the best flavor"
+        description="Using only the best ingredients and baked to perfection, our artisanal bread will give you a taste of Mexico. Our bread is a celebration of Mexican ancestry and culture in addition to being a delicious treat. Experience the flavors that have been handed down through the years by bringing them to your kitchen."
         photo="1.png"
+      />
+      <Product
+        title="Authentic Mexican Bread"
+        description="With our collection of freshly baked bread and pastries, indulge in the lively flavors and rich traditions of Mexico. Whether it's a traditional concha or a flavorful empanada, every bite of our original Mexican pastries offers a sense of tradition. Our tray of Mexican bread and pastries is ideal for any occasion and will take your taste senses right to Mexico. Give your family and friends a delightful taste of Mexico right now."
+        photo="3.png"
+      />
+      <Product
+        title="The Borracho"
+        description="Our genuine borracho bread, with its mouthwatering sweetness and vivid pink swirls, is a Mexican treasure. Anybody with a sweet tooth must try this pastry because it's soft, fluffy, and very delicious. Our borracho bread is the ideal option to help satisfy your cravings whether you're in the mood for a morning snack or an evening dessert. Explore our collection to find a new favorite right away!"
+        photo="2.png"
       />
       <YelpReviews />
       <Footer
--- src/components/Product.js
+++ src/components/Product.js
@@ -6,7 +6,7 @@ function Product(props) {
     <div className="container my-5">
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <img src={props.photo} alt={props.title} className="img-fluid rounded mb-3" />
+        <img src={props.photo} alt={props.title} className="img-fluid rounded mb-3 w-100" />
           <h2 className="text-center">
             <span className="font-weight-bold">{props.title}</span>
             <span className="text-danger">.</span>
